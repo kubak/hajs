d2a4a23ea7e132e2a32ebd237e130c9119781710
kubak <jakub.kwasiborski@adjustyourset.com>
2018-03-20 13:09:47 +0100

    moved cards to a separate file

---
+++ client/src/Cards.js
@@ -0,0 +1,212 @@
+const Cards = [
+   {
+      id : 1,
+      name: 'one',
+      action : (G, ctx, playerID) => {
+         const players = Object.assign({}, ...Object.keys(G.players).map(key => {
+            if (key === playerID) {
+               return {
+                  [key]: {
+                     ...G.players[key],
+                     score: G.players[key].score + 1
+                  }
+               };
+            }
+            return {[key]: { ...G.players[key] } };
+         }));
+         return {
+            ...G,
+            players
+         };
+      },
+      order: 1
+   }, {
+      id : 2,
+      name: 'minus two',
+      action : (G, ctx, playerID) => {
+         const players = Object.assign({}, ...Object.keys(G.players).map(key => {
+            if (key === playerID) {
+               return {
+                  [key]: {
+                     ...G.players[key],
+                     score: Math.max(G.players[key].score - 2, 0)
+                  }
+               };
+            }
+            return {[key]: { ...G.players[key] } };
+         }));
+         return {
+            ...G,
+            players
+         };
+      },
+      order: 2
+   }, {
+      id : 3,
+      name: 'three for everyone',
+      action : (G, ctx, playerID) => {
+         const players = Object.assign({}, ...Object.keys(G.players).map(key => {
+            return {
+               [key]: {
+                  ...G.players[key],
+                  score: G.players[key].score + 3
+               }
+            };
+         }));
+         return {
+            ...G,
+            players
+         };
+      },
+      order: 3
+   }, {
+      id : 4,
+      name: 'four',
+      action : (G, ctx, playerID) => {
+         const players = Object.assign({}, ...Object.keys(G.players).map(key => {
+            if (key === playerID) {
+               return {
+                  [key]: {
+                     ...G.players[key],
+                     score: G.players[key].score + 4
+                  }
+               };
+            }
+            return {[key]: { ...G.players[key] } };
+         }));
+         return {
+            ...G,
+            players
+         };
+      },
+      order: 4
+   }, {
+      id : 5,
+      name: 'half',
+      action : (G, ctx, playerID) => {
+         const players = Object.assign({}, ...Object.keys(G.players).map(key => {
+            if (key === playerID) {
+               return {
+                  [key]: {
+                     ...G.players[key],
+                     score: Math.ceil(G.players[key].score / 2)
+                  }
+               };
+            }
+            return {[key]: { ...G.players[key] } };
+         }));
+         return {
+            ...G,
+            players
+         };
+      },
+      order: 5
+   }, {
+      id : 6,
+      name: 'six',
+      action : (G, ctx, playerID) => {
+         const players = Object.assign({}, ...Object.keys(G.players).map(key => {
+            if (key === playerID) {
+               return {
+                  [key]: {
+                     ...G.players[key],
+                     score: G.players[key].score + 6
+                  }
+               };
+            }
+            return {[key]: { ...G.players[key] } };
+         }));
+         return {
+            ...G,
+            players
+         };
+      },
+      order: 6
+   }, {
+      id : 7,
+      name: 'double',
+      action : (G, ctx, playerID) => {
+         const players = Object.assign({}, ...Object.keys(G.players).map(key => {
+            if (key === playerID) {
+               return {
+                  [key]: {
+                     ...G.players[key],
+                     score: G.players[key].score * 2
+                  }
+               };
+            }
+            return {[key]: { ...G.players[key] } };
+         }));
+         return {
+            ...G,
+            players
+         };
+      },
+      order: 7
+   }, {
+      id : 8,
+      name: 'minus eight',
+      action : (G, ctx, playerID) => {
+         const players = Object.assign({}, ...Object.keys(G.players).map(key => {
+            if (key === playerID) {
+               return {
+                  [key]: {
+                     ...G.players[key],
+                     score: Math.max(G.players[key].score - 8, 0)
+                  }
+               };
+            }
+            return {[key]: { ...G.players[key] } };
+         }));
+         return {
+            ...G,
+            players
+         };
+      },
+      order: 8
+   }, {
+      id : 9,
+      name: 'nine',
+      action : (G, ctx, playerID) => {
+         const players = Object.assign({}, ...Object.keys(G.players).map(key => {
+            if (key === playerID) {
+               return {
+                  [key]: {
+                     ...G.players[key],
+                     score: G.players[key].score + 9
+                  }
+               };
+            }
+            return {[key]: { ...G.players[key] } };
+         }));
+         return {
+            ...G,
+            players
+         };
+      },
+      order: 9
+   }, {
+      id : 10,
+      name: 'ten',
+      action : (G, ctx, playerID) => {
+         const players = Object.assign({}, ...Object.keys(G.players).map(key => {
+            if (key === playerID) {
+               return {
+                  [key]: {
+                     ...G.players[key],
+                     score: G.players[key].score + 10
+                  }
+               };
+            }
+            return {[key]: { ...G.players[key] } };
+         }));
+         return {
+            ...G,
+            players
+         };
+      },
+      order: 10
+   }
+];
+
+export default Cards;
